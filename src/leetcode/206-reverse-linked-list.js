b1768d2fc375aa4ea47bdb9ba1b79ff88efe00dd
d-valles <davalles27@gmail.com>
2020-07-15 10:13:00 -0600

    Add recrusive solution for 206

--- src/leetcode/206-reverse-linked-list.js
+++ src/leetcode/206-reverse-linked-list.js
@@ -15,15 +15,23 @@ var reverseList = function(head) {
     return null;
 }
 
-let currentNode = head;
-let previousHead = null;
+    let currentNode = head;
+    let previousHead = null;
 
-while (currentNode) {
-    let nextHead = currentNode.next;
-    currentNode.next = previousHead;
-    previousHead = currentNode;
-    currentNode = nextHead;
-}
+    while (currentNode) {
+        let nextHead = currentNode.next;
+        currentNode.next = previousHead;
+        previousHead = currentNode;
+        currentNode = nextHead;
+    }
 
-return previousHead;
+    return previousHead;
+};
+
+var reverseListRecusive = function(head) {
+    if (head == null || head.next == null) return head;
+    let reversed = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return reversed;
 };
