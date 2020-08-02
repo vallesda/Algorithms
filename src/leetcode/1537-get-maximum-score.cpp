57908c1b5d5f431a39614d59b24c15ffb89a9938
d-valles <davalles27@gmail.com>
2020-08-02 11:30:50 -0600

    1537

--- src/leetcode/1537-get-maximum-score.cpp
+++ src/leetcode/1537-get-maximum-score.cpp
@@ -75,3 +75,40 @@ public:
         return sum;
     }
 };
+
+
+class Solution2 {
+public:
+    int maxSum(vector<int>& nums1, vector<int>& nums2) {
+        long score = 0, sum1 = 0, sum2 = 0;
+        long mod = 1e9 + 7;
+
+        int i = 0;
+        int j = 0;
+        while (i < nums1.size() && j < nums2.size()) {            
+            if (nums1[i] == nums2[j]) {
+                score += max(sum1, sum2) + nums1[i];
+                sum1 = 0;
+                sum2 = 0;
+                j++;
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                sum2 += nums2[j++];
+            } else if (nums1[i] < nums2[j]) {
+                sum1 += nums1[i++];
+            }
+        }
+        
+        while (i < nums1.size()) {
+            sum1 += nums1[i++];
+        }
+        
+        while (j < nums2.size()) {
+            sum2 += nums2[j++];
+        }     
+        
+        score += max(sum1, sum2);
+        
+        return score % mod;
+    }
+};
