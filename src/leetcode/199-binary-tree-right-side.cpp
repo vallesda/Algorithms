8b287fa5d5d6a46cd7c9e52f3a9e1e2e4e98d62d
d-valles <davalles27@gmail.com>
2020-08-28 07:59:52 -0600

    199

---
+++ src/leetcode/199-binary-tree-right-side.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public: 
+    unordered_map<int,int> hm;
+    
+    void helper(TreeNode* root, int level) {
+        if (!root) return;
+        
+        hm[level] = root->val;
+        
+        helper(root->left, level + 1);
+        helper(root->right, level + 1);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> right;
+        
+        helper(root, 0);
+        
+        for (int i = 0; i < hm.size(); i++) {
+            right.push_back(hm[i]);
+        }
+        
+        return right;
+    }
+};
